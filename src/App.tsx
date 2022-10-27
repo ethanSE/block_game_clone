20799bdb3f203827844474dfd97097c1e0417862
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2022-10-26 20:31:46 -0700

    set up react, typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='website'>
+      <p>This is the website. It has blanched almond</p>
+    </div>
+  );
+}
+
+export default App;
