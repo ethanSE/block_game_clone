1c604d7ce40fe23031521e38d252b3ee23890654
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2023-07-04 17:26:32 -0700

    test

--- src/context/GameStateContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { GSReducerType } from '../hooks/useGameState'
-import { GameState } from "../classes/GameState";
-
-const GameStateContext = createContext<GSReducerType>([new GameState(), () => { }]);
-export default GameStateContext
