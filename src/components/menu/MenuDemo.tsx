a86db45ce2f5880c64a1f8c94540f6e07dda951f
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2023-08-17 14:28:14 -0700

    update menu demo board centering

--- src/components/menu/MenuDemo.tsx
+++ src/components/menu/MenuDemo.tsx
@@ -6,6 +6,7 @@ import { BoardState } from "block-game-clone-backend/types/BoardState"
 import { BaseTiles } from "../playArea/BaseTiles"
 import CubesOnBoard from "../playArea/CubesOnBoard"
 import { BoardCell } from "block-game-clone-backend/types/BoardCell"
+import { Vector3 } from "three"
 
 export const MenuDemo = () => {
     return (
@@ -21,12 +22,12 @@ const MenuDemoGameBoard = () => {
     const row1: BoardCell[][] = [[{ type: 'Player', data: 'p1' }, { type: 'Empty' }, { type: 'Empty' }], [{ type: 'Empty' }, { type: 'Player', data: 'p1' }, { type: 'Empty' }], [{ type: 'Empty' }, { type: 'Player', data: 'p1' }, { type: 'Empty' }]]
     const row2: BoardCell[][] = [[{ type: 'Player', data: 'p1' }, { type: 'Player', data: 'p1' }, { type: 'Player', data: 'p2' }], [{ type: 'Empty' }, { type: 'Empty' }, { type: 'Empty' }], [{ type: 'Empty' }, { type: 'Player', data: 'p1' }, { type: 'Empty' }]]
     const row3: BoardCell[][] = [[{ type: 'Player', data: 'p1' }, { type: 'Player', data: 'p2' }, { type: 'Player', data: 'p2' }], [{ type: 'Empty' }, { type: 'Empty' }, { type: 'Player', data: 'p2' }], [{ type: 'Empty' }, { type: 'Player', data: 'p1' }, { type: 'Empty' }]]
-    const demoBoardState: BoardState = { board: { cells: [row1, row2, row3], height_limits: [[3, 3, 3], [3, 3, 3], [3, 3, 3]], center: [2.0, 2.0, 2.0] }, previewed_piece: null };
+    const demoBoardState: BoardState = { board: { cells: [row1, row2, row3], height_limits: [[3, 3, 3], [3, 3, 3], [3, 3, 3]], center: [1.0, 1.0, 1.0] }, previewed_piece: null };
 
     return (
-        <Center>
+        <group position={new Vector3(...demoBoardState.board.center).negate()}>
             <CubesOnBoard boardState={demoBoardState} update={(a) => { }} />
             <BaseTiles boardState={demoBoardState} update={(a) => { }} />
-        </Center>
+        </group>
     )
 }
