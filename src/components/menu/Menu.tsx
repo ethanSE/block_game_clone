d80d34e1665bdfd304b4975ca7959738da65d476
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2023-08-24 11:40:28 -0700

    adjust menu layout for new modes

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,55 +1,36 @@
 import { MenuDemo } from "./MenuDemo"
 import css from '../../styles/Menu.module.css'
 import { Mode } from "../App"
+import { TwoPlayerMap } from "block-game-clone-backend/types/TwoPlayerMap"
 
 export const Menu = (props: { setMode: (m: Mode) => void }) => {
     return (
         <div className={css.menu}>
-            <div className={css.menuDemoContainer}>
-                <MenuDemo />
-            </div>
-
+            <MenuDemo />
             <div className={css.menuOptionsContainer}>
                 <h3>Select game mode:</h3>
                 <div style={{ flex: '1', display: 'flex', width: '100%' }}>
-                    <div style={{ flex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
-                        <button
-                            onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Tower" } })}>
-                            Two Player - Tower
-                        </button>
-                        <button
-                            onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Pyramid" } })}>
-                            Two Player - Pyramid
-                        </button>
-                        <button
-                            onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Wall" } })}>
-                            Two Player - Wall
-                        </button>
-                        <button
-                            onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Stairs" } })}>
-                            Two Player - Stairs
-                        </button>
-                    </div>
-                    <div style={{ flex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
-                        <button
-                            onClick={() => props.setMode({ type: "VSGreedyAI", data: { type: "Tower" } })}>
-                            AI Player - Tower
-                        </button>
-                        <button
-                            onClick={() => props.setMode({ type: "VSGreedyAI", data: { type: "Pyramid" } })}>
-                            AI Player - Pyramid
-                        </button>
-                        <button
-                            onClick={() => props.setMode({ type: "VSGreedyAI", data: { type: "Wall" } })}>
-                            AI Player - Wall
-                        </button>
-                        <button
-                            onClick={() => props.setMode({ type: "VSGreedyAI", data: { type: "Stairs" } })}>
-                            AI Player - Stairs
-                        </button>
-                    </div>
+                    <MapModeSelector setMode={props.setMode} gameMode={"VSGreedyAI"} />
+                    <MapModeSelector setMode={props.setMode} gameMode={"TwoPlayer"} />
                 </div>
             </div>
+        </div>
+    )
+};
+
+function MapModeSelector(props: { setMode: (m: Mode) => void, gameMode: "VSGreedyAI" | "TwoPlayer" }) {
+    let maps: TwoPlayerMap[] = [{ type: "Tower" }, { type: "Pyramid" }, { type: "Wall" }, { type: "Stairs" }];
+    return (
+        <div className={css.mapSelector}>
+            <h3>{props.gameMode}</h3>
+            {maps.map((map) =>
+                <button
+                    className={css.button}
+                    onClick={() => props.setMode({ type: props.gameMode, data: map })}
+                >
+                    {map.type}
+                </button>
+            )}
         </div>
     )
 }
