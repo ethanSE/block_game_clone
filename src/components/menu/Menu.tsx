bd0ff19169cd090c8c9320168768a1af5dcf97d3
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2023-08-23 20:05:27 -0700

    incorporate greedy ai

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -11,22 +11,44 @@ export const Menu = (props: { setMode: (m: Mode) => void }) => {
 
             <div className={css.menuOptionsContainer}>
                 <h3>Select game mode:</h3>
-                <button
-                    onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Tower" } })}>
-                    Two Player - Tower
-                </button>
-                <button
-                    onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Pyramid" } })}>
-                    Two Player - Pyramid
-                </button>
-                <button
-                    onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Wall" } })}>
-                    Two Player - Wall
-                </button>
-                <button
-                    onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Stairs" } })}>
-                    Two Player - Stairs
-                </button>
+                <div style={{ flex: '1', display: 'flex', width: '100%' }}>
+                    <div style={{ flex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
+                        <button
+                            onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Tower" } })}>
+                            Two Player - Tower
+                        </button>
+                        <button
+                            onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Pyramid" } })}>
+                            Two Player - Pyramid
+                        </button>
+                        <button
+                            onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Wall" } })}>
+                            Two Player - Wall
+                        </button>
+                        <button
+                            onClick={() => props.setMode({ type: "TwoPlayer", data: { type: "Stairs" } })}>
+                            Two Player - Stairs
+                        </button>
+                    </div>
+                    <div style={{ flex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
+                        <button
+                            onClick={() => props.setMode({ type: "VSGreedyAI", data: { type: "Tower" } })}>
+                            AI Player - Tower
+                        </button>
+                        <button
+                            onClick={() => props.setMode({ type: "VSGreedyAI", data: { type: "Pyramid" } })}>
+                            AI Player - Pyramid
+                        </button>
+                        <button
+                            onClick={() => props.setMode({ type: "VSGreedyAI", data: { type: "Wall" } })}>
+                            AI Player - Wall
+                        </button>
+                        <button
+                            onClick={() => props.setMode({ type: "VSGreedyAI", data: { type: "Stairs" } })}>
+                            AI Player - Stairs
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
